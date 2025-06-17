f6121bdfc2f68eda7e75119c1cefd221b5edc60f
Darisahmeti9 <166027089+Darisahmeti9@users.noreply.github.com>
2025-06-17 07:12:17 -0700

    Add files via upload

--- functions.php
+++ functions.php
@@ -8,47 +8,55 @@ function load_scripts(){
         array(),
         filemtime(get_template_directory().'/style.css'),'all'
     );
-    
+
+
+    wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap.min.css');
+
+
     wp_enqueue_script('dropdown',get_template_directory().'/js/dropdown.js',array(),'1.0',false);
+
+
+    wp_enqueue_script('bootstrap2',get_template_directory().'/js/boostrap.bundle.min.js',array('jquery'));
+
 
 }
 
 
 add_action('wp_enqueue_scripts','load_scripts');
 
-function register_menus() {
+
+function config() {
     register_nav_menus(
         array(
-            'wp_devs_main_menu' =>'Main Menu',
-            'wp_devs_footer_menu' =>'Footer Menu'
+            'wp_devs_main_menu'=>'Main Menu',
+            'wp_devs_footer_menu'=>'Footer Menu'
         )
-    );
+        );
 
-    $args = array(
-        'height'=>225,
-        'width'=>1920,
-    );
-    add_theme_support('custom-header', $args);
-    add_theme_support('post-thumbnails');
-    add_theme_support('custom-logo', array(
-        'height' => 200,
-        'width' => 110,
-        'flex-height' => true,
-        'flex-width' => true,
-    ));
-    add_theme_support('automatic-feed-links');
-    add_theme_support('html5', array(
-        'search-form',
-        'comment-form',
-        'comment-list',
-        'gallery',
-        'caption',
-    ));
-    add_theme_support('title-tag');
-}
 
-add_action('after_setup_theme', 'config', 0);
+        $args=array(
+            'height'=>225,
+            'width'=>1920
+        );
 
+
+        add_theme_support('custom-header',$args);
+        add_theme_support('post_thumbnails');
+        add_theme_support('custom-logo',array(
+            'width'=>200,
+            'height'=>110,
+            'flex-height'=>true,
+            'flex-width'=>true
+        ));
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5',array('comment-list','comment-form','search-form','gallery','caption','style','script'));
+        add_theme_support('title-tag');
+
+
+    }
+
+
+    add_action('after_setup_theme','config',0);
 
 
 
